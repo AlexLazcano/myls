1387a31b13805248dda3fea4d43bb4b928da62c9
Alex <alazcano2000@gmail.com>
2021-04-03 22:07:39 -0700

    lexographical order complete

--- myls.c
+++ myls.c
@@ -9,7 +9,7 @@
 #include <time.h>
 // #include <fcntl.h>
 
-void printLastModified(struct dirent *entry, char* path)
+void printLastModified(char *entryName, char *path)
 {
     char filePath[100];
     struct stat sb;
@@ -18,7 +18,7 @@ void printLastModified(struct dirent *entry, char* path)
 
     strcpy(filePath, path);
     strcat(filePath, "/");
-    strcat(filePath, entry->d_name);
+    strcat(filePath, entryName);
 
     // printf("%s\n", filePath);
 
@@ -28,25 +28,129 @@ void printLastModified(struct dirent *entry, char* path)
 
     strftime(tmbuf, sizeof tmbuf, "%b %d %Y %H:%M", date);
 
-    printf("%s %s\n", tmbuf, entry->d_name);
+    printf("%s %s\n", tmbuf, entryName);
 }
 
-void showls(char *path)
+void swap(int i, int j, char **array)
+{
+    char *temp = array[i];
+
+    array[i] = array[j];
+    array[j] = temp;
+}
+
+int w1_less_w2(char *word1, char *word2)
+{
+
+    int minlength = strlen(word1) < strlen(word2) ? strlen(word1) : strlen(word2);
+
+    // printf("word 1: %s\n", word1);
+    // printf("word 2: %s\n", word2);
+    for (int i = 0; i < minlength; i++)
+    {
+        if (word1[i] < word2[i])
+        {
+            // printf("%s Goes first\n", word2);
+            return true;
+        }
+    }
+    return false;
+}
+
+// int w1_more_w2(char *word1, char *word2)
+// {
+
+//     int minlength = strlen(word1) < strlen(word2) ? strlen(word1) : strlen(word2);
+
+//     // printf("word 1: %s\n", word1);
+//     // printf("word 2: %s\n", word2);
+//     for (int i = 0; i < minlength; i++)
+//     {
+//         if (word1[i] > word2[i])
+//         {
+//             // printf("%s Goes first\n", word2);
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+void printArray(char **names, int numberOfName)
+{
+    printf("Name array:\n");
+    for (int i = 0; i < numberOfName; i++)
+    {
+        printf("%d %s\n", i, names[i]);
+    }
+}
+
+int partition(char **array, int left, int right)
+{
+    int pivot = right;
+    int index = left - 1;
+
+    // printf("pivot = %s\n", array[pivot]);
+    for (int i = left; i < right; i++)
+    {
+        if (w1_less_w2(array[i], array[pivot]))
+        {
+            index++;
+            swap(index, i, array);
+        }
+    }
+
+    swap(index + 1, right, array);
+    return index + 1;
+}
+
+void sortLexographically(char **names, int left, int right)
+{
+
+    if (left < right)
+    {
+
+        int q = partition(names, left, right);
+        // printf("partition %d\n", q);
+        sortLexographically(names, left, q - 1);
+        sortLexographically(names, q + 1, right);
+    }
+}
+
+void sort(char **names, int numberOfNames)
+{
+
+    printArray(names, numberOfNames);
+
+    sortLexographically(names, 0, numberOfNames - 1);
+    printArray(names, numberOfNames);
+}
+
+void showDir(char *path)
 {
     DIR *folder;
     struct dirent *entry;
     printf("Path is: %s\n", path);
-    folder = opendir(".");
-    
-    
+
+    folder = opendir(path);
+
     if (folder)
     {
+        int entryLength = 10;
+        char **entryNames = malloc(sizeof(char *) * entryLength);
+        int numberOfEntries = 0;
+
         while (entry = readdir(folder))
         {
-            printLastModified(entry, path);
-            // printf("%s\n", entry->d_name);
-            // printf("%ld\n", entry->d_off);
+            entryNames[numberOfEntries] = entry->d_name;
+            numberOfEntries++;
+            if (numberOfEntries > entryLength)
+            {
+                entryLength += 10;
+                entryNames = realloc(entryNames, sizeof(char *) * entryLength);
+            }
         }
+
+        sort(entryNames, numberOfEntries);
     }
     else
     {
@@ -88,7 +192,7 @@ int main(int argc, char const *argv[])
         }
     }
 
-    showls(".");
+    showDir(PATH);
 
     return 0;
 }
